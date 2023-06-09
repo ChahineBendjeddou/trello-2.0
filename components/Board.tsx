0efec2bcfd094f22d571b8bc99df8e22165fed1d
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-09 21:11:35 +0100

    the board component

---
+++ components/Board.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useBoardStore } from "@/store/BoardStore";
+import { useEffect } from "react";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import Column from "./Column";
+export default function Board() {
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  function handleOnDragEnd(result: DropResult) {}
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+            className="grid grid-cols-1 gap-5 p-5 mx-auto md:grid-cols-3 max-w-7xl"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} todos={column.todos} index={index} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+}
