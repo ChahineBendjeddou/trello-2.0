9aebed9d5c99f282afdbe3dd017f8c8ed2761894
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-09 21:10:31 +0100

    the board type

---
+++ store/BoardStore.ts
@@ -0,0 +1,16 @@
+import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn";
+import { create } from "zustand";
+interface BoardState {
+  board: Board;
+  getBoard: () => void;
+}
+
+export const useBoardStore = create<BoardState>((set) => ({
+  board: {
+    columns: new Map<TypedColumn, Column>(),
+  },
+  getBoard: async () => {
+    const board = await getTodosGroupedByColumn();
+    set({ board });
+  },
+}));
