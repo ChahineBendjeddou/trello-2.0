60f8a12fadef8d3b1135017f72c794a4dac6dbb1
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-10 20:11:06 +0100

    add in the modal to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/Modal";
 import "./globals.css";
 
 export const metadata = {
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-[#f5f6f8]">{children}</body>
+      <body className="bg-[#f5f6f8]">
+        {children}
+        <Modal />
+      </body>
     </html>
   );
 }
