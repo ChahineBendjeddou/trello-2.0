f9b91f1ea4c9d5afb7c9c757aa08a276363d21fc
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-10 20:11:29 +0100

    fixing a ui bug

--- components/Header.tsx
+++ components/Header.tsx
@@ -72,9 +72,9 @@ const Header = () => {
       <div className="flex items-center justify-center px-5 py-2 md:py-5">
         <p className="flex items-center text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl p-5 text-[#0055d1]">
           <UserCircleIcon
-            className={`inline-block h-10 w-10 text-[#0055d1] mr-1 ${
-              loading && "animate-spin"
-            }`}
+            className={`${
+              !loading && "hidden md:inline-block"
+            } h-10 w-10 text-[#0055d1] mr-1 ${loading && "animate-spin"} `}
           />
           {suggestion && !loading
             ? suggestion
